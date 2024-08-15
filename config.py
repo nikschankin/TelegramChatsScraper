48b60a0f97e58bd2c2f90b76738176feb1f85091
n1xon <nikschankin@gmail.com>
2024-08-16 00:39:49 +0300

    .

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-API_ID = ''
-API_HASH = ''
+API_ID = '22870683'
+API_HASH = '88d2752786b49ed6da51da4514f80c27'
 SESSION = ''
-DESTINATION = ''  # получатель
-CHATS = ('chat_name',)  # кортеж из телеграм чатов, которые мы будем парсить
-KEY_WORDS = ('python',) # ключевые слова которые должны быть в сообщении
+DESTINATION = 'https://t.me/+_ykqxM0isVBkNjAy'  # получатель
+CHATS = ('g_jobbot',)  # кортеж из телеграм чатов, которые мы будем парсить
+KEY_WORDS = ('bpmn',) # ключевые слова которые должны быть в сообщении
